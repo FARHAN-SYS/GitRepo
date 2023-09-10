815e4861cf0766b71c9b20df7879cf019f545cc2
FARHAN-SYS <online21c@hotmail.com>
2023-09-11 02:11:38 +0500

    updated Repo

--- 01_Basic/test.js
+++ 01_Basic/test.js
@@ -1,1 +1,12 @@
-console.log("Farhan Ahmed 'Software Developer'");
+document.getElementById("login-form").addEventListener("submit", function (e) {
+  e.preventDefault(); // Prevent the form from actually submitting
+
+  // Get the values entered by the user
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // You can add your authentication logic here, such as sending a request to a server
+
+  // For this example, we'll just display an alert
+  alert(`Username: ${username}\nPin: ${password}`);
+});
