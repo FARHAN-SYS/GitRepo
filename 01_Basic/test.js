9b2dbb1b605e196038a5cd01f53c9d33138cdd8c
FARHAN-SYS <online21c@hotmail.com>
2023-09-11 16:16:59 +0500

    changes for img

--- 01_Basic/test.js
+++ 01_Basic/test.js
@@ -1,12 +1,12 @@
-document.getElementById("login-form").addEventListener("submit", function (e) {
-  e.preventDefault(); // Prevent the form from actually submitting
+// document.getElementById("login-form").addEventListener("submit", function (e) {
+//   e.preventDefault(); // Prevent the form from actually submitting
 
-  // Get the values entered by the user
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+//   // Get the values entered by the user
+//   const username = document.getElementById("username").value;
+//   const password = document.getElementById("password").value;
 
-  // You can add your authentication logic here, such as sending a request to a server
+//   // You can add your authentication logic here, such as sending a request to a server
 
-  // For this example, we'll just display an alert
-  alert(`Username: ${username}\nPin: ${password}`);
-});
+//   // For this example, we'll just display an alert
+//   alert(`Username: ${username}\nPin: ${password}`);
+// });
