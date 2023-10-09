4f235c835af201a99f098423708f571981d78d9d
FARHAN-SYS <online21c@hotmail.com>
2023-10-09 23:20:06 +0500

    Interface update

--- 01_Basic/Web_basic.js
+++ 01_Basic/Web_basic.js
@@ -1,3 +1,10 @@
+// function bag_button() {
+//   alert("Item(s) has been added to Bag");
+// }
+
 function bag_button() {
-  alert("Item(s) has been added to Bag");
+  let cartQuantity = 0;
+  document.querySelector(
+    "#cart-summary"
+  ).innerText = `Your bag has ${cartQuantity} Items`;
 }
