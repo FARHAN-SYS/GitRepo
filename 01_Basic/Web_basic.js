dae8e6229679e8f89bb0d8cc7cf8dd54dd9bf8dd
FARHAN-SYS <online21c@hotmail.com>
2023-10-10 22:02:47 +0500

    updt 10-10-23

--- 01_Basic/Web_basic.js
+++ 01_Basic/Web_basic.js
@@ -1,10 +1,3 @@
 // function bag_button() {
 //   alert("Item(s) has been added to Bag");
 // }
-
-function bag_button() {
-  let cartQuantity = 0;
-  document.querySelector(
-    "#cart-summary"
-  ).innerText = `Your bag has ${cartQuantity} Items`;
-}
